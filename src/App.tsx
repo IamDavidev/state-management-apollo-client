d95e7c1c57b58b9810710332a663d6e2ef38b1d6
itsDavidev <davidlez2mgk@gmail.com>
2022-05-05 10:56:23 -0400

    @apollo-client - cache writing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
 import './App.css';
 import {
   ApolloClient,
@@ -5,8 +8,30 @@ import {
   gql,
   ApolloProvider,
   useQuery,
+  makeVar,
 } from '@apollo/client';
-// import { useEffect } from 'react';
+
+const tasks = {
+  task: {
+    title: '',
+    body: '',
+  },
+};
+
+const davtionState = {
+  user: {
+    name: '',
+    email: '',
+    token: '',
+    _id: '',
+    photo: '',
+    isLogged: false,
+  },
+  tasks: [],
+};
+
+const davtionStateVar = makeVar(davtionState);
+const tasksVar = makeVar(tasks);
 
 export const EXCHANGE_RATES = gql`
   query GetExchangeRates {
@@ -17,32 +42,95 @@ export const EXCHANGE_RATES = gql`
   }
 `;
 
+export const GET_STATE_DAVTION = gql`
+  query GetSateDavtion {
+    davtionState @client {
+      tasks
+      user {
+        name
+        email
+        token
+      }
+    }
+    task @client
+  }
+`;
+// mutation task
+export const AddTask = gql`
+  mutation AddTask($task: TaskInput!) {
+    addTask(task: $task) {
+      id
+      task
+    }
+  }
+`;
+
 const client = new ApolloClient({
   uri: 'https://48p1r2roz4.sse.codesandbox.io',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          davtionState: {
+            read() {
+              return davtionStateVar();
+            },
+          },
+          task: {
+            read() {
+              return tasksVar();
+            },
+          },
+        },
+      },
+    },
+  }),
+  connectToDevTools: true,
 });
 
 function App() {
-  const { data, loading, error } = useQuery(EXCHANGE_RATES);
+  const { data, loading, error } = useQuery(GET_STATE_DAVTION);
 
   if (loading) return <p>laoding ...</p>;
   if (error) return <p>error ...</p>;
   if (!data) return <p>no data ...</p>;
 
+  console.log(data);
+
   return (
     <div className="App">
-      <ApolloProvider client={client}>
-        react - apollo - local state
-        {data?.rates.map(({ currency, rate }: any) => {
-          <div key={currency}>
-            <p>
-              {currency}:{rate}
-            </p>
-          </div>;
-        })}
-      </ApolloProvider>
+      <h1>react - apollo - local state</h1>
+      {/* {data.user.name || ' no user '} */}
     </div>
   );
 }
+
+function Form() {
+  const hadleTask = (e: any) => {
+    e.preventDefault();
+    console.log(e.target.value);
+    return tasksVar({
+      task: {
+        title: e.target.title.value,
+        body: e.target.title.value,
+      },
+    });
+  };
+
+  return (
+    <form onSubmit={hadleTask}>
+      <input type="text" name="title" />
+      <button type="submit">add task</button>
+    </form>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ApolloProvider client={client}>
+    <App />
+    <Form />
+  </ApolloProvider>
+);
+
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,133 +1,63 @@
-import ReactDOM from 'react-dom/client';
-
-import './index.css';
-import './App.css';
 import {
   ApolloClient,
   InMemoryCache,
-  gql,
+  makeVar,
   ApolloProvider,
   useQuery,
-  makeVar,
+  gql,
 } from '@apollo/client';
-
-const tasks = {
-  task: {
-    title: '',
-    body: '',
-  },
-};
-
-const davtionState = {
-  user: {
-    name: '',
-    email: '',
-    token: '',
-    _id: '',
-    photo: '',
-    isLogged: false,
-  },
-  tasks: [],
-};
-
-const davtionStateVar = makeVar(davtionState);
-const tasksVar = makeVar(tasks);
-
-export const EXCHANGE_RATES = gql`
-  query GetExchangeRates {
-    rates(currency: "USD") {
-      currency
-      rate
-    }
+import { hasOwn } from '@apollo/client/cache/inmemory/helpers';
+import { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+const IS_LOGGED_IN = gql`
+  query IsLoggedIn {
+    isLoggedIn @client
   }
 `;
+const isLogged: any = makeVar([]);
 
-export const GET_STATE_DAVTION = gql`
-  query GetSateDavtion {
-    davtionState @client {
-      tasks
-      user {
-        name
-        email
-        token
-      }
-    }
-    task @client
-  }
-`;
-// mutation task
-export const AddTask = gql`
-  mutation AddTask($task: TaskInput!) {
-    addTask(task: $task) {
-      id
-      task
-    }
-  }
-`;
+const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          davtionState: {
-            read() {
-              return davtionStateVar();
-            },
-          },
-          task: {
-            read() {
-              return tasksVar();
-            },
-          },
-        },
-      },
-    },
-  }),
-  connectToDevTools: true,
+  cache,
+  uri: 'http://localhost:4000/graphql',
 });
 
+cache.writeQuery({
+  query: IS_LOGGED_IN,
+  data: {
+    isLoggedIn: isLogged(),
+  },
+});
 function App() {
-  const { data, loading, error } = useQuery(GET_STATE_DAVTION);
+  const { data } = useQuery(IS_LOGGED_IN);
 
-  if (loading) return <p>laoding ...</p>;
-  if (error) return <p>error ...</p>;
-  if (!data) return <p>no data ...</p>;
+  function hadleLogged() {
+    const newItesm = { isLogged: true };
+
+    client.writeQuery({
+      query: IS_LOGGED_IN,
+      data: {
+        isLoggedIn: isLogged([...data.isLoggedIn, newItesm]),
+      },
+    });
+    console.log('logged', isLogged());
+    console.log('>>data', data);
+  }
 
   console.log(data);
 
   return (
     <div className="App">
       <h1>react - apollo - local state</h1>
-      {/* {data.user.name || ' no user '} */}
+      <button onClick={hadleLogged}>button </button>
     </div>
-  );
-}
-
-function Form() {
-  const hadleTask = (e: any) => {
-    e.preventDefault();
-    console.log(e.target.value);
-    return tasksVar({
-      task: {
-        title: e.target.title.value,
-        body: e.target.title.value,
-      },
-    });
-  };
-
-  return (
-    <form onSubmit={hadleTask}>
-      <input type="text" name="title" />
-      <button type="submit">add task</button>
-    </form>
   );
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ApolloProvider client={client}>
     <App />
-    <Form />
   </ApolloProvider>
 );
